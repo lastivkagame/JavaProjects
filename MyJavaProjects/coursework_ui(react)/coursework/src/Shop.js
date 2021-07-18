d733b31e587d9f7e39c203bad3668e3fb4ea55a8
lastivka <lastivkagame@gmail.com>
2021-07-18 19:18:52 +0300

    hm react

--- MyJavaProjects/coursework_ui(react)/coursework/src/Shop.js
+++ MyJavaProjects/coursework_ui(react)/coursework/src/Shop.js
@@ -1,47 +1,46 @@
-// import React, {useState, useEffect} from 'react';
-// import './App.css';
-// import { Button } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// //import { setEnvironmentData } from 'worker_threads';
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { setEnvironmentData } from 'worker_threads';
+import {Link} from 'react-router-dom'
 
-// function Shop() {
-//     useEffect(()=>{
-//         fetchItems();
-//         // fetch('https://fakestoreapi.com/products/')
-//         //     .then(res=> {
-//         //         if(res.ok){
-//         //            return res.json();
-//         //         }
-//         //         throw res;
-//         //     })
-//         //     .then(json=>console.log(json)); 
-//         // console.log(data);
-//     }, []);
+function Shop() {
+    useEffect(()=>{
+        fetchItems();
+       
+    }, []);
 
-//     const [items, setItems] = useState([]);
+    const [items, setItems] = useState([]);
 
-//     compo
     
-//     const fetchItems =  async () => {
-//         const data = await 
-//         fetch('https://fakestoreapi.com/products/')
-//         .then(res=>res.json())
+    
+    const fetchItems =  async () => {
+        const data = await 
+        fetch('https://fakestoreapi.com/products/categories/'); ///categories .. products/ 
+        //.then(res=>res.json())
         
-//             .then(json=>console.log(json)); ////fetch('https://fakestoreapi.com/products/');
-//         console.log(data);
-//     }
-//         //.then(data=>this.setState({clients: data}))
-//         //const items = await data.json();
-//         //console.log(items);
-//         //setItems(items);
-//     }
-//   return (
-//    <div className="App">
-//        {/* {items.map(item=>(
-//            <h1>{item.name}</h1>
-//        ))} */}
-//    </div>
-//   );
-// }
+           // .then(json=>console.log(json)); ////fetch('https://fakestoreapi.com/products/');
 
-// export default Shop;
+        const items = await data.json();
+        console.log(items);
+        setItems(items);
+    }
+        //.then(data=>this.setState({clients: data}))
+        //const items = await data.json();
+        //console.log(items);
+        //setItems(items);
+    
+  return (
+   <div className="App">
+       {items.map(item=>(
+        //    <h1 key={item}>{item}</h1>
+        
+           <Link key={item} to={`/Shop/${item}`}><h1>{item}</h1></Link>
+        //    <h1 key={item.id}>{item.category}</h1>
+       ))}
+   </div>
+  );
+}
+
+export default Shop;
