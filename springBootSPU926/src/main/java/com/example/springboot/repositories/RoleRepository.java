b2bf33aa1dfc8b6975d7557284d10dfebe1de509
Annonymous <dev@rivne.itstep.org>
2021-07-24 15:02:05 +0300

    spring boot security

---
+++ springBootSPU926/src/main/java/com/example/springboot/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.springboot.repositories;
+
+
+import com.example.springboot.entities.Role;
+import com.example.springboot.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
+    Role findRolename(String username);
+}
